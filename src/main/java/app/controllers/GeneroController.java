be6338521dd4b5f947c6e12947a850196151a347
Marco Antônio Gusmão Carvalho <marcocarvalho.web@gmail.com>
2026-03-03 19:47:43 +0000

    Aula 03/03/2026

--- src/main/java/app/controllers/GeneroController.java
+++ src/main/java/app/controllers/GeneroController.java
@@ -11,12 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.PathVariable;
 
-import app.model.Genero;
 import app.record.GeneroDTO;
 import app.record.GeneroInsertDTO;
-import app.repository.GeneroRepository;
+import app.services.GeneroService;
 
-import java.util.Optional;
 import org.springframework.web.bind.annotation.PutMapping;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -25,34 +23,30 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 @RequestMapping("/generos")
 public class GeneroController {
     @Autowired
-    private GeneroRepository generoRepo;
+    private GeneroService generoService;
 
     @GetMapping
     public Iterable<GeneroDTO> list() {
-        return generoRepo.findAll().stream().map(GeneroDTO::new).toList();
+        return generoService.findAll();
     }
 
     @GetMapping("/{id}")
-    public Genero getOne(@PathVariable long id) {
-        return generoRepo.findById(id).get();
+    public GeneroDTO getOne(@PathVariable long id) {
+        return generoService.findOne(id);
     }
 
     @PostMapping
     public GeneroDTO insert(@RequestBody GeneroInsertDTO novaGenero) {
-        Genero novo = new Genero();
-        novo.setNome(novaGenero.nome());
-        return new GeneroDTO(generoRepo.save(novo));
+        return generoService.insert(novaGenero);
     }
 
     @PutMapping("/{id}")
-    public Genero update(@PathVariable long id, @RequestBody Genero modif){
-        Optional<Genero> busca = generoRepo.findById(id);
-        busca.get().setNome(modif.getNome());
-        return generoRepo.save(busca.get());
+    public GeneroDTO update(@PathVariable long id, @RequestBody GeneroInsertDTO modif){
+        return generoService.update(id, modif);
     }
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable long id) {
-        generoRepo.deleteById(id);
+        generoService.delete(id);
     }
 }
