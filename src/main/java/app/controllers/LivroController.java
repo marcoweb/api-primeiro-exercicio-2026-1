929016bd03267146909b67bd8975dac45c1f1dcc
Marco Antônio Gusmão Carvalho <marcocarvalho.web@gmail.com>
2026-03-04 19:27:42 +0000

    Aula 04/03/2026

--- src/main/java/app/controllers/LivroController.java
+++ src/main/java/app/controllers/LivroController.java
@@ -4,8 +4,13 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import app.record.LivroDTO;
+import app.record.LivroInsertDTO;
 import app.services.LivroService;
 
 @RestController
@@ -17,5 +22,17 @@ public class LivroController {
     @GetMapping
     public Iterable<LivroDTO> getAll() {
         return livroService.findAll();
+    }
+
+    @PostMapping
+    public LivroDTO insert(@RequestBody LivroInsertDTO dados) {
+        return livroService.insert(dados);
+    }
+
+    @PutMapping
+    public LivroDTO update(@PathVariable long id, @RequestBody LivroInsertDTO dados) {
+        System.out.println("--------------");
+        System.out.println(id);
+        return livroService.update(id, dados);
     }
 }
--- src/main/java/app/services/LivroService.java
+++ src/main/java/app/services/LivroService.java
@@ -12,31 +12,49 @@ import app.record.LivroDTO;
 import app.record.LivroInsertDTO;
 import app.repository.GeneroRepository;
 import app.repository.LivroRepository;
+import app.services.GeneroService;
 
 @Service
 public class LivroService {
     @Autowired
     private LivroRepository livroRepo;
     @Autowired
-    private GeneroRepository generoRepo;
+    private GeneroService generoService;
+    // private GeneroRepository generoRepo;
 
     public Iterable<LivroDTO> findAll() {
         return livroRepo.findAll().stream().map(LivroDTO::new).toList();
     }
 
     public LivroDTO insert(LivroInsertDTO dados) {
-        Optional<Genero> resultado = generoRepo.findById(dados.id_genero());
+        // Optional<Genero> resultado = generoRepo.findById(dados.id_genero());
         
-        if(resultado.isEmpty()) {
-            throw new ResponseStatusException(
-                HttpStatus.NOT_FOUND, "Gênero Não Encontrado"
-            );
-        }
+        // if(resultado.isEmpty()) {
+        //     throw new ResponseStatusException(
+        //         HttpStatus.NOT_FOUND, "Gênero Não Encontrado"
+        //     );
+        // }
+
+        Genero genero = new Genero(generoService.findOne(dados.id_genero()));
 
         Livro livro = new Livro();
         livro.setTitulo(dados.titulo());
-        livro.setGenero(resultado.get());
+        livro.setGenero(genero);
 
         return new LivroDTO(livroRepo.save(livro));
+    }
+
+    public LivroDTO update(long id, LivroInsertDTO dados) {
+        Optional<Livro> resultado = livroRepo.findById(id);
+        if(resultado.isEmpty()) {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Livro Não Encontrado"
+            );
+        }
+
+        Genero genero = new Genero(generoService.findOne(dados.id_genero()));
+        resultado.get().setTitulo(dados.titulo());
+        resultado.get().setGenero(genero);
+        return new LivroDTO(livroRepo.save(resultado.get()));
     }
 }
