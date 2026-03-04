1cd351eefe679ab897f7e7d69f246ed09a7ca759
Marco Antônio Gusmão Carvalho <marcocarvalho.web@gmail.com>
2026-03-04 18:00:09 +0000

    Aula 04/03/2026

--- src/main/java/app/controllers/LivroController.java
+++ src/main/java/app/controllers/LivroController.java
@@ -3,11 +3,13 @@ package app.controllers;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import app.record.LivroDTO;
 import app.services.LivroService;
 
 @RestController
+@RequestMapping("/livros")
 public class LivroController {
     @Autowired
     private LivroService livroService;
